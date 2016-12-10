07c7d17080dc8a7387336232a8eec36aebf8c222
Sébastien Ohleyer <sebastien.ohleyer@gmail.com>
2016-12-10 16:44:55 +0100

    OK pour la localisation

--- add_followers_with_location.py
+++ add_followers_with_location.py
@@ -1,28 +1,46 @@
-from constants import my_id
 import csv
-import time
-import random
-
-from utilitary import *
-api=get_API()
+import tweepy
+from auth.auth_audience import consumer_key_audience,consumer_secret_audience, access_token_audience, access_token_secret_audience
 
 
 # ____save_followers_____________________________________________________
 
-def save_followers_with_location(api, usr_str, location, filename=None):
+def save_followers_with_location(api, usr_str, location, number_of_page,
+                                 filename=None, first_time=False, cursor=-1):
     if not filename:
-        filename = ''.join(['followers_id_', usr_str, '_in_', location])
+        filename = ''.join(['followers_id_', usr_str, '_in_', location,'.csv'])
         print('Dans le fichier:', filename)
-    followers_list = api.followers(usr_str)
-    print('number of followers of', usr_str, ':', len(followers_list))
-    with open(''.join(['datas/followers_id_with_location/', filename]), 'w', encoding='utf-8', errors='ignore') as csvfile:
+
+    followers_list =[]
+    Cursor_pages = tweepy.Cursor(api.followers, id=usr_str, cursor=cursor).pages(number_of_page)
+
+    for page in Cursor_pages:
+        for follower in page:
+            if location in follower.location:
+                followers_list.append(follower.id_str)
+                print(follower.name, ':', follower.location)
+
+    print(Cursor_pages.next_cursor)
+    print('number of followers of', usr_str,'in', location, ':', len(followers_list))
+
+    with open(''.join(['datas/followers_id_with_location/', filename]), 'a', encoding='utf-8', errors='ignore') as csvfile:
         fieldnames = ['id']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
-        writer.writeheader()
+
+        if first_time:
+            writer.writeheader()
+
         for follower_id in followers_list:
             writer.writerow({'id': follower_id})
     print("data imported, csv writed")
 
 
 if __name__ == '__main__':
-    save_followers_with_location(api, 'lillefrance', 'Lille')
+    auth = tweepy.OAuthHandler(consumer_key_audience, consumer_secret_audience)
+    auth.set_access_token(access_token_audience, access_token_secret_audience)
+
+    api_audience = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    #OK POUR LANCER
+    first_time = False
+    save_followers_with_location(api_audience, 'lillefrance', 'Lille', number_of_page=10, first_time=first_time, cursor=1549278150658371214)
--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def add_followers_main():
     followers_list = import_data('datas/followers_id_MediacitesLille.csv','id')
     follow(api, followers_list[215:], 'friends_cache.csv')
 
+def add_followers_with_location_main(filename):
+    api = get_API()
+    followers_list = import_data(''.join(['datas/followers_id_with_location/', filename]),'id')
+    follow(api, followers_list[26:], 'friends_cache.csv')
 
 def clean_main():
     api = get_API()
@@ -18,7 +22,7 @@ def clean_main():
     save_friends(api, 'Ruche_io', 'my_friends.csv')
     friends_list = import_data('datas/my_friends.csv','id')
     usr_name_to_id(api, 'do_not_touch_id.csv')
-    clean(api, 'my_followers.csv', friends_list, 100)
+    clean(api, 'my_followers.csv', friends_list, 50)
 
 
 def follow_my_followers_main():
@@ -30,4 +34,4 @@ def follow_my_followers_main():
 
 
 if __name__ == '__main__':
-    clean_main()
+    add_followers_with_location_main(filename='followers_id_lillefrance_in_Lille1')
